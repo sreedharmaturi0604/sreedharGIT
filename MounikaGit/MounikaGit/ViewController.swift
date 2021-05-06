ae9db89fa8aae4f29d3e5450cfa4cac0973d9d1a
Sreedhar <sreedharmaturi3@gmail.com>
2021-05-06 19:09:44 +0530

    create a new mounika file.

---
+++ MounikaGit/MounikaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MounikaGit
+//
+//  Created by ADMINI on 5/6/21.
+//  Copyright © 2021 ADMINI. All rights reserved.
+//change as per local respo
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
